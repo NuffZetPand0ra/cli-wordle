586d34d045a623602c541cd558fc047b95c361f2
NuffZetPand0ra <esbentind@gmail.com>
2024-07-27 19:54:34 +0200

    Slight refactor in db setup script

--- setup_db.php
+++ setup_db.php
@@ -6,20 +6,18 @@ use \PDO;
 use \PDOStatement;
 use \PDOException;
 use Nuffy\wordle\controllers\WordController;
+use Nuffy\wordle\models\Dictionary;
 
 require_once __DIR__.'\vendor\autoload.php';
-
-$dictionaries = [
-    "default" => "A list of 5-letter english words",
-    "danish" => "A list of danish words",
-    "pokemon" => "A list of the first 151 pokemon",
-];
 
 try{
     $db_dir = __DIR__.'\data\wordle.sqlite';
     $pdo = new PDO("sqlite:$db_dir");
 
     $pdo->exec("
+        DROP TABLE IF EXISTS dictionaries;
+        DROP TABLE IF EXISTS words;
+
         CREATE TABLE IF NOT EXISTS dictionaries (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
@@ -34,17 +32,22 @@ try{
         );
     ");
 
-    foreach($dictionaries as $key=>$description){
-        $dict_to_fetch = $key;
-        $dict_description = $description;
-
-        $dict_array = WordController::getDictionary($dict_to_fetch)->getWords();
+    $dictionaries_to_load = [
+        "default" => "A list of 5-letter english words",
+        "danish" => "A list of danish words",
+        "pokemon" => "A list of the first 151 pokemon",
+    ];
+    
+    foreach($dictionaries_to_load as $dictionary_to_load=>$dict_description){
         $pdo->exec("
-            INSERT INTO dictionaries (name, description) VALUES ('$dict_to_fetch', '$dict_description');
+            INSERT INTO dictionaries (name, description) VALUES ('$dictionary_to_load', '$dict_description');
         ");
         $dict_id = $pdo->lastInsertId();
 
-        $dict_chunks = array_chunk($dict_array, 1000);
+        $dict_array = str_getcsv(file_get_contents(__DIR__.'/dictionaries/'.$dictionary_to_load.'.txt'), "\n");
+        $dict_array = array_map('strtoupper', $dict_array);
+
+        $dict_chunks = array_chunk($dict_array, 5000);
 
         foreach($dict_chunks as $dict){
             $values = [];
@@ -58,7 +61,7 @@ try{
             $word_statement = $pdo->prepare($sql);
             $word_statement->execute($params);
         }
-        echo "Inserted $key dictionary.\n";
+        echo "Inserted $dictionary_to_load dictionary.\n";
     }
 
 
